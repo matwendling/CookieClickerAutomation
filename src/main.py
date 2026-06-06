dccc3f4a86c13f93e37e115a749d32352d421ba1
kurtwendling <mateusfagundeslopes@gmail.com>
2026-06-06 13:12:03 -0500

    Tests made - Changed from find_element to find_elements

--- src/main.py
+++ src/main.py
@@ -7,17 +7,34 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import threading
 
 driver_version = os.getenv('UNDETECTED_CHROMEDRIVER_V')
 driver = uc.Chrome(version_main=int(driver_version))
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 wait = WebDriverWait(driver, 20)
 
-language_menu = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[2]/div[12]/div/div[1]/div[1]/div[2]")))
+language_menu = wait.until(EC.element_to_be_clickable((By.ID, "langSelect-EN")))
 language_menu.click()
 
-cookie = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div[2]/div[15]/div[8]/button")))
-cookie.click()
+cookie = wait.until(EC.element_to_be_clickable((By.ID, "bigCookie")))
+
+# TESTING AREA
+
+buildings = driver.find_elements(By.CSS_SELECTOR, "#store div")
+building_ids = [building.get_attribute("id") for building in buildings[:-1]]
+
+# DEF GET ALL AVAILABLE PRODUCTS 
+for building in buildings:
+    if building.get_attribute("class") == "product locked disabled":
+        print(building.get_attribute("id"))
+
+while True:
+    try:
+        cookie.click()  
+        time.sleep(0.1) 
+    except: 
+        print("Couldn't click the cookie.")
 
 time.sleep(10)
 
